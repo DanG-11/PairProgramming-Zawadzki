50580b74b47d4935518a7799041c221d746221a8
Mango67 <4422807@TE-GDA-D-D21E03>
2026-03-30 11:10:42 +0200

    + Klasa Field z pustym konstruktorem

---
+++ PairProgramming-Zawadzki/Field.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairProgramming_Zawadzki
+{
+    internal class Field
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool Active { get; set; }
+
+        public Field()
+        {
+
+        }
+    }
+}
