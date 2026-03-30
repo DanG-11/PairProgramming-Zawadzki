d11f2777f0468365c07c5ef1d88aee2d5679e770
Mango67 <4422807@TE-GDA-D-D21E03>
2026-03-30 11:39:53 +0200

    Podstawy ruchu i planszy

--- PairProgramming-Zawadzki/MainWindow.xaml.cs
+++ PairProgramming-Zawadzki/MainWindow.xaml.cs
@@ -13,12 +13,51 @@ namespace PairProgramming_Zawadzki
 {
     public partial class MainWindow : Window
     {
-        int[,] playerPos = new int[10, 10];
+        int[,] gameBoard = new int[10, 10];
+        int[] fellaPos = new int[]{0, 0};
         public MainWindow()
         {
             InitializeComponent();
+
+            DisplayBoard();
         }
 
+        private void DisplayBoard()
+        {
+            GameBoardGrid.Children.Clear();
 
+            Label fellaLabel = new Label();
+            fellaLabel.Content = "Ziutek";
+            Grid.SetRow(fellaLabel, fellaPos[0]);
+            Grid.SetColumn(fellaLabel, fellaPos[1]);
+
+            GameBoardGrid.Children.Add(fellaLabel);
+        }
+
+        private void Move(object sender, KeyEventArgs e)
+        {
+            string key = e.Key.ToString();
+
+            if (key == "Up")
+            {
+                fellaPos[0]--;
+                DisplayBoard();
+            }
+            else if (key == "Down")
+            {
+                fellaPos[0]++;
+                DisplayBoard();
+            }
+            else if (key == "Left")
+            {
+                fellaPos[1]--;
+                DisplayBoard();
+            }
+            else if (key == "Right")
+            {
+                fellaPos[1]++;
+                DisplayBoard();
+            }
+        }
     }
 }
